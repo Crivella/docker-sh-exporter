aaf0bac766d30dd40e84f9e435e8db1fe5cac825
Crivella <davidecrivella@yahoo.it>
2023-01-30 17:42:31 +0100

    Improved customizability. Added possible inotifywait daemon

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,33 @@
 FROM debian:buster
 
-RUN apt-get update
 RUN apt-get update \
     && apt-get install -y --no-install-recommends \
         cron \
         wget \
-        ca-certificates 
+        ca-certificates \
+        duplicity \
+        inotify-tools
 
 ENV \
     SCRIPT_DIR="/scripts" \
     COLLECT_DIR="/data" \
+    MONITOR_DIR="/backups" \
     CRON_SCHEDULE="*/5 * * * *" \
+    SCRIPT_REGEX="^.*\.sh$$" \
+    RUN_SCRIPTS_ON_STARTUP="NO" \
+    INOTIFY_ENABLE="YES" \
+    INOTIFY_LOG_FILE="/data/inotify.log" \
+    INOTIFY_FMT="%T %e %w %f" \
+    INOTIFY_TIMEFMT="%Y-%m-%d %H:%M:%S %z" \
+    INOTIFY_EVENTS="CREATE" \
+    EXTRA_INSTALL="" \
     NODE_EXPORTER_VERSION="1.3.1" \
     NODE_EXPORTER_ARCH="linux-amd64" \
     NODE_EXPORTER_LISTEN_ADDRESS="0.0.0.0" \
     NODE_EXPORTER_LISTEN_PORT=9781 
 
 VOLUME ${SCRIPT_DIR}
+VOLUME ${MONITOR_DIR}
 
 EXPOSE 9781/tcp
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
 
-echo "Installing node-exporter"
-mkdir -p /node-exporter
-cd /node-exporter
+EXTRA_INSTALL=`echo ${EXTRA_INSTALL} | tr -s "," " "`
+apt-get update && apt-get install -y --no-install-recommends ${EXTRA_INSTALL} 
 
 DESTDIR=node_exporter-${NODE_EXPORTER_VERSION}.${NODE_EXPORTER_ARCH}
-FILE=${DESTDIR}.tar.gz
-wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/${FILE}
-tar -zxvf ${FILE}
+if [ ! -e /node-exporter/${DESTDIR}/node_exporter ]; then
+    echo "Installing node-exporter"
+    mkdir -p /node-exporter
+    cd /node-exporter
+
+    FILE=${DESTDIR}.tar.gz
+    wget https://github.com/prometheus/node_exporter/releases/download/v${NODE_EXPORTER_VERSION}/${FILE}
+    tar -zxvf ${FILE}
+fi
 
 echo "Creating data collection dir"
 mkdir -p ${COLLECT_DIR}
 
 echo "Make sure scripts are executables"
-find ${SCRIPT_DIR} -name "*.sh" -exec chmod +x {} \;
+find ${SCRIPT_DIR} -regex "${SCRIPT_REGEX}" -exec chmod +x {} \;
 echo "Adding run_dir to cron"
-echo "${CRON_SCHEDULE} root run-parts --regex=.*\.sh ${SCRIPT_DIR}" > /etc/cron.d/exporter
+echo "${CRON_SCHEDULE} root run-parts --regex=${SCRIPT_REGEX} ${SCRIPT_DIR}" > /etc/cron.d/exporter
 
-run-parts --regex=.*\.sh ${SCRIPT_DIR}
+if [[ "${RUN_SCRIPTS_ON_STARTUP^^l}" == "yes" ]]; then
+    echo "Running scripts on startup..."
+    run-parts --test --regex=${SCRIPT_REGEX} ${SCRIPT_DIR}
+    run-parts --regex=${SCRIPT_REGEX} ${SCRIPT_DIR}
+fi
+if [[ "${INOTIFY_ENABLE^^l}" == "yes" ]]; then
+    echo "Starting inotifywait daemon..."
+    INOTIFY_EVENTS="`echo ${INOTIFY_EVENTS} | tr -s "," " "`"
+    OPT=""
+    for e in ${INOTIFY_EVENTS}; do
+        OPT+="-e ${e^^} "
+    done
+    inotifywait -m -r -d -o ${INOTIFY_LOG_FILE} --format "${INOTIFY_FMT}" --timefmt "${INOTIFY_TIMEFMT}" ${OPT} ${MONITOR_DIR}
+fi
 
+echo "Starting cron daemon..."
 cron -L 4
 
+echo "Starting node exporter..."
 /node-exporter/${DESTDIR}/node_exporter \
     --web.listen-address="${NODE_EXPORTER_LISTEN_ADDRESS}:${NODE_EXPORTER_LISTEN_PORT}" \
     --collector.disable-defaults \
